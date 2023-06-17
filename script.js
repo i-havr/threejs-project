a5271c3be1405201d328f74b7e4fe991d027a590
i-havr <i.havrylov@gmail.com>
2023-06-17 17:54:25 +0300

    first settings

---
+++ script.js
@@ -0,0 +1,40 @@
+import * as THREE from "three";
+
+//Every project must contain the next four elements:
+// * Scene
+// * Mesh(all the objects)
+// * Camera
+// * Renderer
+
+//Scene
+const scene = new THREE.Scene();
+
+//Mesh
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: "purple" });
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+//Camera
+const aspect = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+const camera = new THREE.PerspectiveCamera(
+  75,
+  aspect.width / aspect.height,
+  1, //near value
+  2000 //far value
+);
+camera.position.z = 3;
+camera.position.x = 1;
+camera.position.y = 1;
+
+scene.add(camera);
+
+//Renderer
+const canvas = document.querySelector(".draw"); // select the canvas element
+const renderer = new THREE.WebGLRenderer({ canvas }); // add the WebGLRenderer
+renderer.setSize(aspect.width, aspect.height); // Renderer size
+renderer.render(scene, camera); // display what the camera in the scene captured
